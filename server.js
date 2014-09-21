0ce077f130943c6d4f20dfe8fbe314a4f0a10ecd
flemdizzle <fleminsh@gmail.com>
2014-09-21 19:38:17 -0400

    added loop for running more bars + used appendFile

--- server.js
+++ server.js
@@ -4,51 +4,56 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-app.get('/scrape', function(req, res){
+// app.get('/scrape', function(req, res){
 	//The magic happens here
 
-	//url anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
-
-	request(url, function(error, response, html){
-
-		if(!error){
-
-			var $ = cheerio.load(html);
-
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+	//url
+	var bars = ["iota-club-and-cafe-arlington", "galaxy-hut-arlington", "world-of-beer-arlington-2", "the-liberty-tavern-arlington"]
+	for (i = 0; i <= bars.length-1; i++) {
+		url = 'http://www.yelp.com/biz/' + bars[i];
 		
-			$('.header').filter(function(){
+		request(url, function(error, response, html){
 
-				var data = $(this);
+			if(!error){
 
-				title = data.children().first().text();
-				release = data.children().last().text();
+				var $ = cheerio.load(html);
 
-				json.title = title;
-				json.release = release;
+				var name, address, hours;
+				var json = { name : "", address : "", hours : ""};
+			
+				$('.biz-page-title').filter(function(){
+					var data = $(this);
+					name = data.text();
+
+					json.name = name.replace(/(\s+|\n)/gm," ");
+				})
+
+				$('.street-address').filter(function(){
+					var data = $(this);
+
+					address = data.text();
+					json.address = address.replace(/(\s+|\n)/gm," ");
+				})
+
+				$('.hours-table').filter(function(){
+					var data = $(this);
+
+					hours = data.text();
+					json.hours = hours.replace(/(\s+|\n)/gm," ");
+				})
+			}
+			fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err){
+				console.log('File successfully written! Check for output.json file.');
 			})
-
-			$('.star-box-giga-star').filter(function(){
-				var data = $(this);
-
-				rating = data.text();
-				json.rating = rating;
-			})
-		}
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-			console.log('File successfully written! Check for output.json file.');
+			
+			// res.send('Check your console!')
 		})
-		
-		res.send('Check your console!')
-	})
+	}
 
+// });
 
-});
+// app.listen('3000');
 
-app.listen('8081');
+// console.log('Sever running on port 3000');
 
-console.log('Sever running on port 8081');
-
-exports = module.exports = app;
+// exports = module.exports = app;
