4cb6bd2b50d2d3585332548e7a32753e300ad904
flemdizzle <fleminsh@gmail.com>
2014-09-21 19:46:36 -0400

    took out server functionality

--- server.js
+++ server.js
@@ -4,56 +4,44 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-// app.get('/scrape', function(req, res){
-	//The magic happens here
+//url
+var bars = ["iota-club-and-cafe-arlington", "galaxy-hut-arlington", "world-of-beer-arlington-2", "the-liberty-tavern-arlington"]
+for (i = 0; i <= bars.length-1; i++) {
+	url = 'http://www.yelp.com/biz/' + bars[i];
+	
+	request(url, function(error, response, html){
 
-	//url
-	var bars = ["iota-club-and-cafe-arlington", "galaxy-hut-arlington", "world-of-beer-arlington-2", "the-liberty-tavern-arlington"]
-	for (i = 0; i <= bars.length-1; i++) {
-		url = 'http://www.yelp.com/biz/' + bars[i];
+		if(!error){
+
+			var $ = cheerio.load(html);
+
+			var name, address, hours;
+			var json = { name : "", address : "", hours : ""};
 		
-		request(url, function(error, response, html){
+			$('.biz-page-title').filter(function(){
+				var data = $(this);
+				name = data.text();
 
-			if(!error){
-
-				var $ = cheerio.load(html);
-
-				var name, address, hours;
-				var json = { name : "", address : "", hours : ""};
-			
-				$('.biz-page-title').filter(function(){
-					var data = $(this);
-					name = data.text();
-
-					json.name = name.replace(/(\s+|\n)/gm," ");
-				})
-
-				$('.street-address').filter(function(){
-					var data = $(this);
-
-					address = data.text();
-					json.address = address.replace(/(\s+|\n)/gm," ");
-				})
-
-				$('.hours-table').filter(function(){
-					var data = $(this);
-
-					hours = data.text();
-					json.hours = hours.replace(/(\s+|\n)/gm," ");
-				})
-			}
-			fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err){
-				console.log('File successfully written! Check for output.json file.');
+				json.name = name.replace(/(\s+|\n)/gm," ");
 			})
-			
-			// res.send('Check your console!')
+
+			$('.street-address').filter(function(){
+				var data = $(this);
+
+				address = data.text();
+				json.address = address.replace(/(\s+|\n)/gm," ");
+			})
+
+			$('.hours-table').filter(function(){
+				var data = $(this);
+
+				hours = data.text();
+				json.hours = hours.replace(/(\s+|\n)/gm," ");
+			})
+		}
+		fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err){
+			console.log('File successfully written! Check for output.json file.');
 		})
-	}
-
-// });
-
-// app.listen('3000');
-
-// console.log('Sever running on port 3000');
-
-// exports = module.exports = app;
+		
+	})
+}
